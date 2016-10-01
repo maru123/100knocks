d5351197d861a0e9ca905eec3be8dc10227ead82
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2016-10-01 12:54:07 +0900

    03

---
+++ 03.rb
@@ -0,0 +1,5 @@
+
+#"Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+puts "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.".scan(/[^ .,]+/).map {|word|
+  word.size
+}
