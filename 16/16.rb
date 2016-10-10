cabe85fcd68a91eccaf0662a7f1f0bfecdd589d7
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2016-10-10 13:53:53 +0900

    16

--- 16/16.rb
+++ 16/16.rb
@@ -2,3 +2,13 @@
 
 # 確認
 # split -l 3 ../hightemp.txt
+
+def split_file_by_lines file_path, number
+  lines = File.readlines(file_path)
+  lines[0..lines.size.fdiv(number).ceil-1].each_index do |index|
+    puts "-----------------------"
+    puts lines.slice!(0..number-1)
+  end
+end
+
+split_file_by_lines '../hightemp.txt', 5
